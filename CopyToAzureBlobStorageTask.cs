d372c5b6d5dc90f0f074ea4e7251ef3bf744f74e
Rhys Godfrey <rhys.godfrey@gmail.com>
2012-02-26 13:40:01 +0000

    Initial Commit

---
+++ CopyToAzureBlobStorageTask.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.StorageClient;
+using System.IO;
+
+namespace RhysG.MSBuild.Azure
+{
+    public class CopyToAzureBlobStorageTask : ITask
+    {
+        private IBuildEngine _buildEngine;
+        private ITaskHost _taskHost;
+
+        public IBuildEngine BuildEngine
+        {
+            get
+            {
+                return _buildEngine;
+            }
+            set
+            {
+                _buildEngine = value;
+            }
+        }
+
+        [Required]
+        public string ContainerName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string ConnectionString
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string ContentType
+        {
+            get;
+            set;
+        }
+
+        public string ContentEncoding
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public ITaskItem[] Files
+        {
+            get;
+            set;
+        }
+
+        public bool Execute()
+        {
+            CloudStorageAccount.SetConfigurationSettingPublisher((configName, configSetter) =>
+            {
+                // Provide the configSetter with the initial value
+                configSetter(ConnectionString);
+            });
+
+            CloudStorageAccount account = CloudStorageAccount.FromConfigurationSetting("ConnectionSetting");
+
+            CloudBlobClient client = account.CreateCloudBlobClient();
+
+            CloudBlobContainer container = client.GetContainerReference(ContainerName);
+            container.CreateIfNotExist();
+            container.SetPermissions(new BlobContainerPermissions() { PublicAccess = BlobContainerPublicAccessType.Container });
+
+            foreach (ITaskItem fileItem in Files)
+            {
+                FileInfo file = new FileInfo(fileItem.ItemSpec);
+
+                CloudBlob blob = container.GetBlobReference(file.Name);
+
+                try
+                {
+                    blob.FetchAttributes();
+                }
+                catch (StorageClientException) { }
+
+                DateTime lastModified = DateTime.MinValue;
+
+                if (!String.IsNullOrWhiteSpace(blob.Metadata["LastModified"]))
+                {
+                    long timeTicks = long.Parse(blob.Metadata["LastModified"]);
+                    lastModified = new DateTime(timeTicks, DateTimeKind.Utc);
+                }
+
+                if (lastModified != file.LastWriteTimeUtc)
+                {
+                    blob.UploadFile(file.FullName);
+
+                    blob.Properties.ContentType = ContentType;
+
+                    if (!String.IsNullOrWhiteSpace(ContentEncoding))
+                    {
+                        blob.Properties.ContentEncoding = ContentEncoding;
+                    }
+
+                    blob.Metadata["LastModified"] = file.LastWriteTimeUtc.Ticks.ToString();
+                    blob.SetMetadata();
+                    blob.SetProperties();
+
+                    BuildEngine.LogMessageEvent(new BuildMessageEventArgs(String.Format("Updating: {0}", file.Name), String.Empty, "CopyToAzureBlobStorageTask", MessageImportance.Normal));
+                }
+            }
+
+            return true;
+        }
+
+        public ITaskHost HostObject
+        {
+            get
+            {
+                return _taskHost;
+            }
+            set
+            {
+                _taskHost = value;
+            }
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,56 @@
+﻿# MSBuild Copy To Azure Blob Storage Task #
+
+## Introduction ##
+
+A MSBuild task to allow copying of files to Azure Blob Storage. To save unnecessary copying files are
+only copied if the source files last write time is different to the one tagged against the blob in Azure.
+
+## Usage ##
+
+Include the DLL in your MSBuild project
+
+	<UsingTask TaskName="CopyToAzureBlobStorageTask" AssemblyFile="..\PATH-TO-DLL\RhysG.MSBuild.Azure.dll"/>
+
+Create an item group referencing the files you want to copy to Azure Blob storage. For example to copy
+all the javascript files from the *scripts* directory:
+
+	<ItemGroup>
+		<Scripts Include="..\PATH-TO-FILES\scripts\*.js" />
+    </ItemGroup>
+
+Then call the *CopyToAzureBlobStorageTask* this takes several parameters described below. To copy the files
+selected above to a container named *scripts*:
+
+	<CopyToAzureBlobStorageTask ContainerName="scripts" ContentType="text/javascript" ConnectionString="YOUR-AZURE-CONNECTION-STRING" Files="@(Scripts)" />
+
+### Parameters ###
+
+<table>
+	<tr>
+		<th>Parameter</th>
+		<th>Description</th>
+	</tr>
+	<tr>
+		<td>ContainerName</td>
+		<td>The name of the Azure Blob container to copy the files to. If this does not exist it will be created</td>
+	</tr>
+	<tr>
+		<td>ConnectionString</td>
+		<td>The Azure connection string to use</td>
+	</tr>
+	<tr>
+		<td>Files</td>
+		<td>The files to copy</td>
+	</tr>
+	<tr>
+		<td>ContentType</td>
+		<td>*Optional* The content type for Azure to serve the files as</td>
+	</tr>
+</table>
+
+## License ##
+
+This library is released under the *FreeBSD License*, see **LICENSE.txt** for more information.
+
+Blog: http://www.rhysgodfrey.co.uk
+Twitter: @rhysgodfrey
